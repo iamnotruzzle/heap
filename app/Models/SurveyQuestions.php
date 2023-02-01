193034b93e637334d3da39071d38f65a17c48239
iamnotruzzle <iamnotruzzle@gmail.com>
2023-02-01 14:48:08 +0800

    created model and migration for survey questions

---
+++ app/Models/SurveyQuestions.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SurveyQuestions extends Model
+{
+    use HasFactory;
+
+    protected $table = 'key_generator';
+    // protected $primaryKey = 'id';
+    // public $incrementing = false;
+    // declare primary as string
+    // protected $keyType = 'string';
+
+    protected $fillable = [
+        'id',
+        'pss_id',
+        'desc',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:m-d-Y',
+    ];
+}
