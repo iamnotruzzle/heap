b174c3a66b3f92f424abac9f3a73c0f9b174f38e
iamnotruzzle <iamnotruzzle@gmail.com>
2023-02-01 15:07:31 +0800

    created model and migration for hospital staff

---
+++ app/Models/HospitalStaff.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HospitalStaff extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hospital_staff';
+    // protected $primaryKey = 'id';
+    // public $incrementing = false;
+    // declare primary as string
+    // protected $keyType = 'string';
+
+    protected $fillable = [
+        'id',
+        'type',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:m-d-Y',
+    ];
+}
