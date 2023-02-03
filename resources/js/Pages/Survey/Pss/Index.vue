66a480a6182ce0acf0bd0565ebac9ab568095695
iamnotruzzle <iamnotruzzle@gmail.com>
2023-02-03 13:51:27 +0800

    design layout for age, sex and religion

--- resources/js/Pages/Survey/Pss/Index.vue
+++ resources/js/Pages/Survey/Pss/Index.vue
@@ -55,7 +55,7 @@
 
             <v-card-text>
               <v-row>
-                <v-col>
+                <v-col cols="12">
                   <p>Dear <span class="font-weight-bold">Patient/Client,</span></p>
                   <div class="indent">
                     The following questions talk about your overall experience in this hospital. We wish to improve the
@@ -65,6 +65,164 @@
                   </div>
                 </v-col>
               </v-row>
+
+              <v-row>
+                <v-col cols="12">
+                  <p>
+                    <b>Direction:</b> Put a check(<v-icon>mdi-check</v-icon>) mark on the appropriate box of your
+                    answer:
+                  </p>
+                </v-col>
+              </v-row>
+
+              <v-form
+                ref="form"
+                class="no-wrap"
+              >
+                <v-row>
+                  <v-col cols="12">
+                    <p class="font-weight-bold">The Respondent of this Survey Form:</p>
+                    <div class="d-flex flex-column ma-0 pa-0">
+                      <v-checkbox
+                        v-model="respondent"
+                        label="Patient"
+                        value="Patient"
+                        dense
+                        hide-details
+                        class="mx-2"
+                      ></v-checkbox>
+
+                      <v-checkbox
+                        v-model="respondent"
+                        label="Companion(Family/Relative)"
+                        value="Companion"
+                        dense
+                        hide-details
+                        class="mx-2"
+                      ></v-checkbox>
+
+                      <div class="d-flex flex-row mx-2 mt-1">
+                        <v-checkbox
+                          v-model="enableRespondent"
+                          dense
+                        ></v-checkbox>
+                        <v-text-field
+                          v-model="otherRespondent"
+                          :disabled="!enableRespondent"
+                          label="Others (Specify)"
+                          dense
+                        ></v-text-field>
+                      </div>
+                    </div>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <v-divider color="black"></v-divider>
+                  </v-col>
+
+                  <!-- age -->
+                  <v-col
+                    cols="4"
+                    md="2"
+                  >
+                    <label
+                      for="age"
+                      class="font-weight-bold"
+                      >Age</label
+                    >
+                    <v-text-field
+                      id="age"
+                      v-model="age"
+                      type="number"
+                      hide-spin-buttons
+                      dense
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-spacer></v-spacer>
+
+                  <!-- sex -->
+                  <v-col
+                    cols="8"
+                    md="2"
+                  >
+                    <label
+                      for="sex"
+                      class="font-weight-bold"
+                      >Sex</label
+                    >
+                    <div class="d-flex flex-row">
+                      <v-checkbox
+                        id="sex"
+                        v-model="sex"
+                        label="Male"
+                        value="Male"
+                        dense
+                        hide-details
+                        class="mr-2"
+                      ></v-checkbox>
+                      <v-checkbox
+                        id="sex"
+                        v-model="sex"
+                        label="Female"
+                        value="Female"
+                        dense
+                        hide-details
+                      ></v-checkbox>
+                    </div>
+                  </v-col>
+
+                  <v-spacer></v-spacer>
+                  <!-- religion -->
+                  <v-col
+                    cols="12"
+                    md="6"
+                  >
+                    <label
+                      for="religion"
+                      class="font-weight-bold"
+                      >Religion
+                    </label>
+
+                    <div class="d-flex">
+                      <v-checkbox
+                        v-model="religion"
+                        label="Catholic"
+                        value="Catholic"
+                        dense
+                        hide-details
+                        class="mr-2"
+                      ></v-checkbox>
+
+                      <v-checkbox
+                        v-model="religion"
+                        label="Muslim"
+                        value="Muslim"
+                        dense
+                        hide-details
+                        class="mr-2"
+                      ></v-checkbox>
+
+                      <div class="d-flex flex-row">
+                        <v-checkbox
+                          v-model="enableReligion"
+                          dense
+                        ></v-checkbox>
+                        <v-text-field
+                          v-model="otherReligion"
+                          :disabled="!enableReligion"
+                          label="Others (Specify)"
+                          dense
+                        ></v-text-field>
+                      </div>
+                    </div>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <v-divider color="black"></v-divider>
+                  </v-col>
+                </v-row>
+              </v-form>
             </v-card-text>
           </v-card>
         </v-layout>
@@ -76,7 +234,40 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      respondent: '',
+      enableRespondent: false,
+      otherRespondent: '',
+      age: '',
+      sex: '',
+      religion: '',
+      enableReligion: false,
+      otherReligion: '',
+    };
+  },
+  watch: {
+    respondent: function (val) {
+      if (this.respondent != '') {
+        this.otherRespondent = '';
+        this.enableRespondent = false;
+      }
+    },
+    enableRespondent: function (val) {
+      if (this.enableRespondent == true) {
+        this.respondent = '';
+      }
+    },
+    religion: function (val) {
+      if (this.religion != '') {
+        this.otherReligion = '';
+        this.enableReligion = false;
+      }
+    },
+    enableReligion: function (val) {
+      if (this.enableReligion == true) {
+        this.religion = '';
+      }
+    },
   },
 };
 </script>
@@ -98,9 +289,14 @@ export default {
 
 .indent {
   text-indent: 50px;
+  text-align: justify;
 }
 
 .large-screen {
   max-width: 1000px !important;
+}
+
+.no-wrap {
+  white-space: no-wrap;
 }
 </style>
