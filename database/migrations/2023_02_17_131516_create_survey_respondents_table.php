4c867a6adcc7747451f5dbc8331231990fa87baf
iamnotruzzle <iamnotruzzle@gmail.com>
2023-02-17 13:59:29 +0800

    created validation to check if patients hosp number is already recorded

---
+++ database/migrations/2023_02_17_131516_create_survey_respondents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveyRespondentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('sqlsrv2')->create('survey_respondents', function (Blueprint $table) {
+            $table->id();
+            $table->string('paacctno');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('sqlsrv2')->dropIfExists('survey_respondents');
+    }
+}
