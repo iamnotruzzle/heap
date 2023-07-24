0d73d0110c0b5a5af23460984f4ee526c52704b3
iamnotruzzle <iamnotruzzle@gmail.com>
2023-07-24 13:27:04 +0800

    created user locations table

---
+++ database/migrations/2023_07_24_132345_create_user_locations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('sqlsrv2')->create('user_locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('wardcode');
+            $table->bigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('sqlsrv2')->dropIfExists('user_locations');
+    }
+}
