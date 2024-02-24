f96ff89e99e6fb19fdfc05fd66392c1837a7cc93
iamnotruzzle <iamnotruzzle@gmail.com>
2024-02-24 11:21:12 +0800

    breakpoint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             HospitalStaffSeeder::class,
             SurveyEnglishQuestionsSeeder::class,
             SurveyIlocoQuestionsSeeder::class,
-            SurveyTagalogQuestions::class,
+            SurveyTagalogQuestionsSeeder::class,
             LocationsSeeder::class,
         ]);
     }
